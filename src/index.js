dea40b63d2bc7d3c190521a7fc43032f64f70090
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-09-07 16:25:45 +0200

    Add coloring to sidebar styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./css/styles.css";
+
+// Convert images to css masks to enable recoloring of svg images
+document.querySelectorAll("img.auto-mask-icon")
+    .forEach(img => {
+        const imageSource = img.getAttribute("src");
+        img.style.maskImage = `url(${imageSource})`;
+    });
