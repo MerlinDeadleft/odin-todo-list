fb63e7aff3d421823e173aded19d76af396d6787
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-09-08 20:06:09 +0200

    Add event dispatcher class

---
+++ src/core/EventDispatcher.js
@@ -0,0 +1,29 @@
+class EventDispatcher {
+    #handlers = [];
+
+    addEventListener(callback) {
+        this.#handlers.push(callback);
+
+        return () => removeEventListener(callback);
+    }
+
+    removeEventListener(callback) {
+        let index = this.#handlers.indexOf(callback);
+        while(index >= 0) {
+            this.#handlers.splice(index, 1);
+            index = this.#handlers.indexOf(callback);
+        }
+    }
+
+    dispatchEvent(...payload) {
+        this.#handlers.forEach(handler => {
+            try {
+                handler(...payload);
+            } catch(error) {
+                console.error("An event listener threw an error during execution: ", error);
+            }
+        })
+    }
+}
+
+export { EventDispatcher }
