8981df2948438878b92b7c8ed7e06ccccbd65c08
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-09-11 20:45:17 +0200

    Register all classes with ioc container

--- src/container.js
+++ src/container.js
@@ -1,5 +1,24 @@
 import { IocContainer } from "./core/iocContainer.js";
+import { ProjectsModel } from "./models/projectsModel.js";
+import { ModalDialogService } from "./services/modalDialogService.js";
+import { ModalFactory } from "./factories/modalFactory.js";
+import { SidebarController } from "./controllers/sidebarController.js";
+import { AddProjectController } from "./controllers/addProjectController.js";
+import { SidebarView } from "./views/sidebarView.js";
+import { AddProjectView } from "./views/addProjectView.js";
 
 const container = new IocContainer();
+
+container.registerSingleton(ProjectsModel);
+
+container.registerSingleton(ModalDialogService);
+
+container.registerSingleton(ModalFactory);
+
+container.registerTransient(SidebarController)
+container.registerTransient(AddProjectController)
+
+container.registerTransient(SidebarView);
+container.registerTransient(AddProjectView);
 
 export { container }
